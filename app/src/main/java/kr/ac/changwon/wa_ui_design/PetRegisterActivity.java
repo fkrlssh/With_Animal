4069f2fae69bafc84a71400a23539d0d51668154
rlatndu <ksyksjsym@gmail.com>
2024-12-11 21:48:52 +0900

    발견신고 사진,위치,전화번호 입력 가능, 문자입력에러 예외처리 추가

--- app/src/main/java/kr/ac/changwon/wa_ui_design/PetRegisterActivity.java
+++ app/src/main/java/kr/ac/changwon/wa_ui_design/PetRegisterActivity.java
@@ -101,6 +101,10 @@ public class PetRegisterActivity extends AppCompatActivity {
                     petSpeciesEditText.setError("종을 입력하세요");
                     isValid = false;
                 }
+                else if (!isValidAge(petAge)) { // 나이 유효성 검사
+                    petAgeEditText.setError("나이는 숫자만 입력 가능합니다.");
+                    isValid = false;
+                }
 
                 if (petAge.isEmpty()) {
                     petAgeEditText.setError("나이를 입력하세요");
